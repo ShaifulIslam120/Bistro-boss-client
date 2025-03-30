29708d65677b388d955f27838c9f561f249965a5
shaiful <shaifulcit@gmil.com>
2025-03-30 23:22:28 +0600

    salad Start

---
+++ src/pages/Catagories/SoupCata.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import chefService from '../../assets/home/chef-service.jpg';
+import { motion } from 'framer-motion'; // Added missing import
+import CoverSection from '../reusable/CoverSection';
+import UseMenu from '../hooks/UseMenu';
+const SoupCata = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SoupCata;
